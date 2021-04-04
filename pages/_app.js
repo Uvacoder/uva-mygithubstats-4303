e168b01beefbf3efd95d3b5aaca108281b4ad9cd
Noel Delgado <auhcsei@gmail.com>
2021-04-04 17:57:24 -0600

    style(*) main layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="app-wrapper">
+      <main className="app-main">
+        <Component {...pageProps} />
+      </main>
+      <footer className="app-footer">
+        [global footer]
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
