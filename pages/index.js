3255d58651227206700843f166d01ea998f3f4a2
Noel Delgado <auhcsei@gmail.com>
2021-04-04 22:04:06 -0600

    feat(search) add basic functionality and display results

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
+import { useState } from 'react';
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
+const { log, error } = console;
+
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+
+    const q = searchTerm.trim();
+    if (!q) return;
+
+    try {
+      const res = await fetch(`/api/search?q=${q}`);
+      const data = await res.json();
+      setSearchResults(data.search.nodes);
+    } catch (err) {
+      error(err);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,13 +35,43 @@ export default function Home() {
         Enter GitHub username
       </p>
 
-      <form>
+      <form
+        onSubmit={handleFormSubmit}
+      >
         <input
           type="text"
           placeholder="ex. 'noeldelgado'"
           className={styles.input}
+          value={searchTerm}
+          onChange={(ev) => setSearchTerm(ev.target.value)}
         />
       </form>
+
+      <ul className={styles.list}>
+        {searchResults.map((user) => {
+          return (
+            <li key={user.id}>
+              <Link href={`/user/${user.login}`}>
+                <a>
+                  <div>
+                    <img
+                      src={user.avatarUrl}
+                      alt={user.login + ' avatar'}
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div>
+                    <p><b>{user.login}</b></p>
+                    <small>{user.name}</small>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
     </div>
   )
 }
