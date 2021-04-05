a08ecee255cbf282fda3c91322c36931e3f537d8
Noel Delgado <auhcsei@gmail.com>
2021-04-05 17:18:41 -0600

    feat(api) add user/[user]

---
+++ pages/api/user/[user].js
@@ -0,0 +1,148 @@
+import { fetchGQL } from '../../../util/api';
+
+const { log } = console;
+
+async function getRepositoriesByUser(login, limit = 30, after = null) {
+  if (after) after = `\"${after}\"`;
+
+  return await fetchGQL({query: `
+    {
+      user(login: "${login}") {
+        repositories(first: ${limit}, after: ${after}) {
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+          edges {
+            node {
+              name
+              stargazerCount
+              forkCount
+              primaryLanguage {
+                name
+              }
+              licenseInfo {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  `});
+}
+
+async function getAllRepositoriesByUser(username) {
+  let repos = [];
+
+  const getRepos = async (login, cursor = null) => {
+    log(login, cursor)
+    let { user: { repositories }} = await getRepositoriesByUser(login, 100, cursor);
+    repos = repos.concat(repositories.edges);
+
+    if (repositories.pageInfo.hasNextPage) {
+      await getRepos(username, repositories.pageInfo.endCursor);
+    }
+  };
+  await getRepos(username);
+
+  return { repositories: repos };
+}
+
+export default async function(req, res) {
+  const username = req.query.user;
+
+  const gql = {query: `
+    {
+      user(login: "${username}") {
+        login
+        repositories {
+          totalCount
+        }
+        avatarUrl(size: 200)
+        followers {
+          totalCount
+        }
+        following {
+          totalCount
+        }
+        starredRepositories {
+          totalCount
+        }
+        createdAt
+        bio
+        status {
+          emoji
+          emojiHTML
+          message
+        }
+        location
+        email
+        websiteUrl
+        twitterUsername
+        repositories {
+          totalCount
+        }
+        gists {
+          totalCount
+        }
+        contributionsCollection {
+          totalCommitContributions
+          totalIssueContributions
+          totalPullRequestContributions
+          totalPullRequestReviewContributions
+          contributionCalendar {
+            totalContributions
+            weeks {
+              contributionDays {
+                # date
+                color
+                # contributionCount
+                # contributionLevel
+                weekday
+              }
+            }
+          }
+          commitContributionsByRepository(maxRepositories: 100) {
+            contributions {
+              totalCount
+            }
+            repository {
+              primaryLanguage {
+                name
+              }
+            }
+          }
+        }
+        packages {
+          totalCount
+        }
+        projects {
+          totalCount
+        }
+        organizations {
+          totalCount
+        }
+        sponsorshipsAsMaintainer(includePrivate:true) {
+          totalCount
+        }
+        sponsorshipsAsSponsor {
+          totalCount
+        }
+        watching {
+          totalCount
+        }
+      }
+    }
+  `};
+
+  const [{ user }, { repositories }] = await Promise.all([
+    fetchGQL(gql),
+    getAllRepositoriesByUser(username)
+  ]);
+
+  res.json({
+    user,
+    repositories
+  });
+}
