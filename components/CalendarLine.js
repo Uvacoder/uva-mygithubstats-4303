3ac7696660c18c316af3e523251cc43448e1b4b1
Noel Delgado <auhcsei@gmail.com>
2021-04-27 12:13:14 -0600

    style(components) CalendarLine: lint

--- components/CalendarLine.js
+++ components/CalendarLine.js
@@ -11,7 +11,7 @@ const internals = {
     }
 
     function getDates(week) {
-      const { 0: first, length, [length-1]: last } = week.dates;
+      const { 0: first, length, [length - 1]: last } = week.dates;
       const dateOptions = { month: 'short', day: 'numeric', year: 'numeric' };
       const firstDate = formatDate(first, dateOptions);
       const lastDate = formatDate(last, dateOptions);
@@ -23,13 +23,13 @@ const internals = {
       <strong>${getContributionText(week)}</strong> on<br/>
       ${getDates(week)}
     `;
-  }
+  },
 };
 
 export default function CalendarLine({
   data: { weeks, months },
   width = 912,
-  height = 180
+  height = 180,
 }) {
   const dotRadius = 3;
   const lineWidth = 1.5;
@@ -37,48 +37,54 @@ export default function CalendarLine({
   const ySpace = 14;
   const graphWidth = width - xSpace;
   const graphHeight = height - ySpace;
-  const contributionsPerWeek = weeks.map(week => {
-    return week.contributionDays.reduce((acc, current) => {
-      acc.total += current.contributionCount;
-      acc.dates.push(current.date);
-      return acc;
-    }, { total: 0, dates: [] });
+  const contributionsPerWeek = weeks.map((week) => {
+    return week.contributionDays.reduce(
+      (acc, current) => {
+        acc.total += current.contributionCount;
+        acc.dates.push(current.date);
+        return acc;
+      },
+      { total: 0, dates: [] },
+    );
   });
-  const monthsLabel = months.map(m => {
-    return [
-      m.name,
-      new Array(m.totalWeeks - 1).fill()
-    ].flat();
-  }).flat();
+  const monthsLabel = months
+    .map((m) => {
+      return [m.name, new Array(m.totalWeeks - 1).fill()].flat();
+    })
+    .flat();
   const totalWeeks = weeks.length;
-  const max = Math.max(...contributionsPerWeek.map(w => w.total));
+  const max = Math.max(...contributionsPerWeek.map((w) => w.total));
 
-  let polygonPoints = `M${-lineWidth},${graphHeight - dotRadius - lineWidth / 2} `;
+  let polygonPoints = `M${-lineWidth},${
+    graphHeight - dotRadius - lineWidth / 2
+  } `;
 
   const printDots = contributionsPerWeek.map((week, i) => {
-    const x = graphWidth / totalWeeks * i;
+    const x = (graphWidth / totalWeeks) * i;
     const y = clamp(
       dotRadius + lineWidth / 2,
-      graphHeight - week.total * height / max || graphHeight,
-      graphHeight - dotRadius - lineWidth / 2
+      graphHeight - (week.total * height) / max || graphHeight,
+      graphHeight - dotRadius - lineWidth / 2,
     );
 
     polygonPoints += `${x},${y} `;
 
-    return <circle
-      key={i}
-      r={dotRadius}
-      cx={x}
-      cy={y}
-      data-total={week.total}
-      strokeWidth={lineWidth}
-      stroke={'var(--color-primary)'}
-      fill='var(--color-background)'
-      opacity={week.total ? 1 : .25}
-      data-tip={internals.getTooltipText(week)}
-      data-html={true}
-      data-effect='solid'
-    />
+    return (
+      <circle
+        key={i}
+        r={dotRadius}
+        cx={x}
+        cy={y}
+        data-total={week.total}
+        strokeWidth={lineWidth}
+        stroke={'var(--color-primary)'}
+        fill="var(--color-background)"
+        opacity={week.total ? 1 : 0.25}
+        data-tip={internals.getTooltipText(week)}
+        data-html={true}
+        data-effect="solid"
+      />
+    );
   });
 
   polygonPoints += `${graphWidth},${graphHeight - dotRadius - lineWidth / 2}`;
@@ -87,56 +93,46 @@ export default function CalendarLine({
     <>
       <svg
         viewBox={`0 0 ${width} ${height}`}
-        preserveAspectRatio='xMaxYMax slice'
+        preserveAspectRatio="xMaxYMax slice"
         width={width}
         height={height}
       >
         {new Array(5).fill(0).map((_, i, arr) => {
           const y = clamp(
             dotRadius + lineWidth / 2,
-            graphHeight - graphHeight/arr.length * (i + 1),
-            graphHeight - dotRadius - lineWidth / 2
+            graphHeight - (graphHeight / arr.length) * (i + 1),
+            graphHeight - dotRadius - lineWidth / 2,
           );
           return (
             <g key={i}>
-              <line
-                x1={0}
-                x2={graphWidth}
-                y1={y}
-                y2={y}
-              />
-              <text
-                x={graphWidth + 4}
-                y={y}
-                dominantBaseline='middle'
-              >
-                {Math.round(max/5*(i+1))}
+              <line x1={0} x2={graphWidth} y1={y} y2={y} />
+              <text x={graphWidth + 4} y={y} dominantBaseline="middle">
+                {Math.round((max / 5) * (i + 1))}
               </text>
             </g>
-        )})}
+          );
+        })}
         <text
           x={graphWidth + 4}
           y={graphHeight - dotRadius - lineWidth / 2}
-          dominantBaseline='middle' >
+          dominantBaseline="middle"
+        >
           {0}
         </text>
 
-        <path d={polygonPoints}/>
+        <path d={polygonPoints} />
 
         {printDots}
 
         {monthsLabel.map((m, i) => {
-          const x = graphWidth / totalWeeks * i;
+          const x = (graphWidth / totalWeeks) * i;
 
           return (
-            <text
-              key={i}
-              x={x}
-              y={height-2}
-            >
+            <text key={i} x={x} y={height - 2}>
               {monthsLabel[i]}
-          </text>
-        )})}
+            </text>
+          );
+        })}
       </svg>
 
       <style jsx>{`
@@ -173,9 +169,9 @@ export default function CalendarLine({
 CalendarLine.propTypes = {
   data: shape({
     weeks: array.isRequired,
-    months: array.isRequired
+    months: array.isRequired,
   }).isRequired,
 
   width: number,
-  height: number
+  height: number,
 };
