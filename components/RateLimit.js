9078355ad3452203e5f136f39a1292d5cf489724
Noel Delgado <auhcsei@gmail.com>
2021-04-27 12:20:52 -0600

    style(components) RateLimit: lint

--- components/RateLimit.js
+++ components/RateLimit.js
@@ -3,24 +3,21 @@ import styles from '~/styles/components/RateLimit.module.css';
 
 export default function RateLimit() {
   const { data: rateLimit, error } = useSWR('/api/rate-limit', {
-    refreshInterval: 3000
+    refreshInterval: 3000,
   });
   const remaining = rateLimit?.remaining;
 
   let status = '';
 
-  if (remaining === 0)
-    status = 'error';
-  else if (remaining <= 100)
-    status = 'warn';
-  else if (remaining > 100)
-    status = 'ok';
+  if (remaining === 0 || error) status = 'error';
+  else if (remaining <= 100) status = 'warn';
+  else if (remaining > 100) status = 'ok';
 
   return (
     <>
       <div className={styles.root} data-status={status}>
-        <svg viewBox='0 0 10 10' width={10} height={10} className='mr05'>
-          <circle r={5} cx={5} cy={5}/>
+        <svg viewBox="0 0 10 10" width={10} height={10} className="mr05">
+          <circle r={5} cx={5} cy={5} />
         </svg>
         {remaining} requests left
       </div>
