a1fdcfd2d389c6cf82cb16aa65ebcb1c727d5526
Noel Delgado <auhcsei@gmail.com>
2021-04-27 12:01:49 -0600

    style(search) lint page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 import SearchInput from '~/components/SearchInput';
 import UserLinkCard from '~/components/UserLinkCard';
-import styles from '~/styles/Home.module.css'
+import styles from '~/styles/Home.module.css';
 
-const { log, error } = console;
+const { error } = console;
 
 const internals = {
   reSpace: new RegExp(/\s/g),
@@ -28,7 +28,7 @@ export default function Home() {
       const data = await res.json();
       setSearchResults({
         search: data.search,
-        term: q
+        term: q,
       });
     } catch (err) {
       error(err);
@@ -41,12 +41,13 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>GitHub Profile Stats</title>
-        <meta name='description' content='gh-profile-stats displays different statistics for GitHub users'/>
+        <meta
+          name="description"
+          content="gh-profile-stats displays different statistics for GitHub users"
+        />
       </Head>
 
-      <p className={styles.description}>
-        Enter GitHub username
-      </p>
+      <p className={styles.description}>Enter GitHub username</p>
 
       <div className={styles.inputWrapper}>
         <SearchInput
@@ -63,7 +64,7 @@ export default function Home() {
           {searchResults.search.nodes.map((user) => {
             return (
               <li key={user.id}>
-                <UserLinkCard data={user}/>
+                <UserLinkCard data={user} />
               </li>
             );
           })}
@@ -72,24 +73,25 @@ export default function Home() {
 
       {Boolean(searchResults?.search?.userCount === 0) && (
         <div className={styles.noSearchResults}>
-          <p className='mb05'>
+          <p className="mb05">
             Your search for “{searchResults.term}” didn’t return any results.
           </p>
-          <p className='secondary-text'>
+          <p className="secondary-text">
             <i>
-              Check the username typing or <br/>visit
-              {' '}
+              Check the username typing or <br />
+              visit{' '}
               <a
                 href={`https://github.com/${searchResults.term}`}
-                rel='noopener noreferrer'
-                target='_blank'>GitHub/{searchResults.term}
-              </a>
-              {' '}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                GitHub/{searchResults.term}
+              </a>{' '}
               to see if the user exists.
             </i>
           </p>
         </div>
       )}
     </div>
-  )
+  );
 }
