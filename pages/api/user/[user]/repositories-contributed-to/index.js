f1820cb648a2913121c5eeaa89cda4e21a44f817
Noel Delgado <auhcsei@gmail.com>
2021-05-02 09:33:41 -0600

    feat(api) user: repositories contributed to

---
+++ pages/api/user/[user]/repositories-contributed-to/index.js
@@ -0,0 +1,44 @@
+import { fetchGQL } from '~/util/api';
+
+export default async function (req, res) {
+  const { user, cursor } = req.query;
+  console.log(user);
+  console.log(cursor);
+  const gql = {
+    query: `
+    {
+      user(login: "${user}") {
+        repositoriesContributedTo(
+          first: 10,
+          after: "${cursor}",
+          includeUserRepositories: true,
+          contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY, PULL_REQUEST_REVIEW],
+          orderBy: { field: PUSHED_AT, direction: DESC }
+        ) {
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+          edges {
+            node {
+              nameWithOwner
+              url
+              isInOrganization
+              owner {
+                login
+                avatarUrl(size: 48)
+              }
+              primaryLanguage {
+                name
+                color
+              }
+            }
+          }
+        }
+      }
+    }`,
+  };
+
+  const data = await fetchGQL(gql);
+  res.json(data);
+}
